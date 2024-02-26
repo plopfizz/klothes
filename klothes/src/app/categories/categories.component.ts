139cef12b31d821bc4c7a40a99dc87c4e0ccc9fe
Mohit mandlecha <mohitmandlecha7256@gmail.com>
2024-02-27 01:22:57 +0530

    Adding navbars to the code

---
+++ klothes/src/app/categories/categories.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.scss'
+})
+export class CategoriesComponent {
+
+}
--- klothes/src/app/main-component/main.module.ts
+++ klothes/src/app/main-component/main.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooterModule } from '../footer/footer.module';
+import { HeaderComponentModule } from '../header/header-component/header-component.module';
+import { HomeComponent } from '../home/home.component';
+import { KidsComponent } from '../kids/kids.component';
+import { MenComponent } from '../men/men.component';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { WomenComponent } from '../women/women.component';
 import { MainComponentComponent } from './main-component.component';
-import { HeaderComponentModule } from '../header/components/header-component/header-component.module';
+import { WishlistComponent } from '../wishlist/wishlist.component';
+import { UserLoginComponent } from '../user-login/user-login.component';
+import { BagComponent } from '../bag/bag.component';
+import { CategoriesComponent } from '../categories/categories.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'men', component: MenComponent },
+  { path: 'women', component: WomenComponent },
+  { path: 'kids', component: KidsComponent },
+  { path: 'wishlist', component: WishlistComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'bag', component: BagComponent },
 
-
+  { path: '**', component: PageNotFoundComponent },
+];
 @NgModule({
   declarations: [MainComponentComponent],
   imports: [
     CommonModule,
-    HeaderComponentModule
+    RouterModule.forRoot(routes),
+    HeaderComponentModule,
+    FooterModule,
+    CategoriesComponent
   ],
-  exports:[MainComponentComponent]
+  exports: [MainComponentComponent],
 })
-export class MainModule { }
+export class MainModule {}
